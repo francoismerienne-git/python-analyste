583b8099c1910acc16122fe703eb92f57bf5c4a8
François Mérienne <francois.merienne@gmail.com>
2026-09-23 17:48:01 +0200

    M3 : nettoyage des 3 mois, consolidation et indicateurs

--- m3.py
+++ m3.py
@@ -1,5 +1,8 @@
+# ===== JANVIER =====
+
 import pandas as pd
 import numpy as np
+
 
 #import + encoding + séparateur + skpi rows
 janvier = pd.read_csv("donnees/export_janvier.csv",encoding="cp1252",sep=";",skiprows=2)
@@ -23,9 +26,9 @@ janvier = janvier.drop_duplicates()
 #print(janvier.duplicated().sum())
 
 
-"""
-Index(['Date', 'Canal ', 'Dépenses (€)', 'Clics', 'Conversions'], dtype='str')
-"""
+
+#Index(['Date', 'Canal ', 'Dépenses (€)', 'Clics', 'Conversions'], dtype='str')
+
 
 #Rename columns
 janvier = janvier.rename(columns={"Date":"date","Canal ":"canal","Dépenses (€)":"depenses","Clics":"clics","Conversions":"conversions"})
@@ -44,5 +47,80 @@ janvier["depenses"] = pd.to_numeric(janvier["depenses"],errors="coerce")
 #Clean date format
 janvier["date"] = pd.to_datetime(janvier["date"],format="%d/%m/%Y")
 
-print(janvier.isna())
-print(janvier.isna().sum())
+#Export to clean csv
+janvier.to_csv("sorties/janvier_propre.csv",index=False)
+
+
+
+# ===== FEVRIER =====
+
+fevrier = pd.read_csv("donnees/export_fevrier.csv",sep=",")
+
+#From english to french
+fevrier = fevrier.rename(columns={"channel":"canal","spend":"depenses","clicks":"clics"})
+
+#Lower case
+fevrier["canal"] = fevrier["canal"].str.lower().str.strip()
+
+#Deduplicate
+fevrier = fevrier.drop_duplicates()
+
+#Clean date format
+fevrier["date"] = pd.to_datetime(fevrier["date"],format="%Y-%m-%d")
+
+#Export to clean csv
+fevrier.to_csv("sorties/fevrier_propre.csv",index=False)
+""
+
+
+# ===== MARS =====
+
+mars = pd.read_csv("donnees/export_mars.csv",sep="\t")
+
+#Rename columns
+mars = mars.rename(columns={"Canal":"canal","Jour":"date","Conversions":"conversions","Clics":"clics","Budget dépensé":"depenses"})
+
+#Clean canal with lower case and spaces
+mars["canal"] = mars["canal"].str.lower().str.strip()
+
+#clean depenses
+mars["depenses"] = mars["depenses"].str.replace(" ","").str.replace(",",".").str.replace("€","")
+
+#Deduplicate
+mars = mars.drop_duplicates()
+
+#Depenses from string to float
+mars["depenses"] = pd.to_numeric(mars["depenses"],errors="coerce")
+
+#Clean date format
+mars["date"] = pd.to_datetime(mars["date"],format="%d/%m/%Y")
+
+#Export to clean csv
+mars.to_csv("sorties/mars_propre.csv",index=False)
+
+"""
+print("----------Mars Cleané----------")
+print(mars)
+
+print("----------Mars Info----------")
+mars.info()
+"""
+
+
+# ===== CONSOLIDATION  =====
+
+
+total = pd.concat([janvier,fevrier,mars],ignore_index=True)
+print("----------Total----------")
+print(total)
+#total.info()
+#print(total["canal"].value_counts())
+
+total["cout_par_conversion"] = (total["depenses"] / total["conversions"]).round(2)
+
+
+par_canal = total.groupby("canal").agg(
+{"depenses": "sum", "clics": "sum", "conversions":"sum"}
+).reset_index()
+
+print(par_canal)
